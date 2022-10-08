c386fbe2b5ec07f2981fc3a580ddd1c15bcf9568
ankushsss <ankushsss99@gmail.com>
2022-10-09 02:17:15 +0530

    thired

--- src/Component/TwitterList.tsx
+++ src/Component/TwitterList.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import { Table, TableRow, TableCell, TableBody, TableContainer, TableHead, Avatar, Button, TextField} from '@mui/material'
+import { Table, TableRow, TableCell, TableBody, TableContainer, TableHead, Avatar, Button, TextField } from '@mui/material'
 import { type } from '@testing-library/user-event/dist/type'
 
 
 // import makeStyles from '@emotion/styled'
 type twittList = {
-    author:string
-    _id:string
-    url:string
-    likes:number
-    imageUrl:string
-    
+    author: string
+    _id: string
+    url: string
+    likes: number
+    imageUrl: string
+
 }
 type res = {
     data: []
@@ -58,70 +58,66 @@ export const TwitterList = () => {
 
     useEffect(() => {
         getData()
-       let store:any =localStorage.getItem("store")
+        let store: any = localStorage.getItem("store")
         let storeParse = JSON.parse(store)
         console.log(storeParse)
         setLocalStore(storeParse)
 
     }, [])
-    const searchUser = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        let filter:Array<twittList> = []
-     twittList.map((matching:twittList)=>{
-        let key = matching._id
-        if(matching.author.search(e.target.value) != -1)
-        {
-          filter.push(matching)
-        }
-        // return matching.author.match(e.target.value) 
-     })
+    const searchUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let filter: Array<twittList> = []
+        twittList.map((matching: twittList) => {
+            let key = matching._id
+            if (matching.author.search(e.target.value) != -1) {
+                filter.push(matching)
+            }
+            // return matching.author.match(e.target.value) 
+        })
 
         console.log(filter)
-        if(e.target.value != "")
-        {
-        setTweetList(filter)
+        if (e.target.value != "") {
+            setTweetList(filter)
 
         }
-        else{
+        else {
             getData()
         }
-        
+
     }
 
-    const filterByDate = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        let filter:Array<twittList> = []
-        
+    const filterByDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let filter: Array<twittList> = []
 
-        let filterData =  searchDate.map((matching:any)=>{
+
+        let filterData = searchDate.map((matching: any) => {
             let val = new Date(e.target.value).toString()
             let lengthStr = matching.publishedDate.length
             let date = new Date(matching.publishedDate).toString()
-            console.log(date.slice(0,15) == val.slice(0,15))
-           if(date.slice(0,15) == val.slice(0,15))
-           {
-             filter.push(matching)
-           }
+            console.log(date.slice(0, 15) == val.slice(0, 15))
+            if (date.slice(0, 15) == val.slice(0, 15)) {
+                filter.push(matching)
+            }
 
 
-           // return matching.author.match(e.target.value) 
+            // return matching.author.match(e.target.value) 
         })
-        if(e.target.value != "")
-        {
-        setTweetList(filter)
-        
+        if (e.target.value != "") {
+            setTweetList(filter)
+
 
         }
-        else{
+        else {
             getData()
         }
     }
 
-    async function setInLocalStorege(like:any){
-        if(localStore.length == 0){
+    async function setInLocalStorege(like: any) {
+        if (localStore.length == 0) {
             setLocalStore(like)
         }
-        setLocalStore([...localStore,like]) 
+        setLocalStore([...localStore, like])
         localStorage.setItem("store", JSON.stringify(localStore));
-      
+
     }
 
 
@@ -138,56 +134,59 @@ export const TwitterList = () => {
     }
     return (
         <div>
-            <TextField placeholder='search' onChange={searchUser}/>
-            <TextField type="date" onChange={filterByDate}/>
-            <Button variant='contained' onClick={()=>  {
+            <div style={{ width: "100%",textAlign:"center" }}><h1>Twitter Twitt List</h1></div>
+
+            <TextField placeholder='search....' onChange={searchUser}  sx={{marginRight:"10px"}}/>
+            <TextField type="date" onChange={filterByDate} sx={{marginRight:"10px"}}/>
+            <Button variant='contained' onClick={() => {
                 localStorage.setItem("store", JSON.stringify([]))
                 setLocalStore([])
-                }}>Clear Local Storage</Button>
-                            <TableContainer>
-                                <Table aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Author</TableCell>
-                                            <TableCell align="left">Link</TableCell>
-                                            <TableCell align="left">Like</TableCell>
-                                            <TableCell align="left">Action</TableCell>
+            }}>Clear Local Storage</Button>
+
+            <TableContainer>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Author</TableCell>
+                            <TableCell align="left">Link</TableCell>
+                            <TableCell align="left">Like</TableCell>
+                            <TableCell align="left">Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            twittList.map((twitt: any) => {
+                                let isTrue
+                                localStore.map((compair: any) => {
+                                    if (twitt._id == compair._id) {
+                                        isTrue = true
+                                    }
+
+                                })
+                                return (
+                                    <>
+                                        <TableRow key={twitt._id}>
+                                            <TableCell ><Avatar src={twitt.imageUrl} />{twitt.author}</TableCell>
+                                            <TableCell><a href={twitt.url}>{twitt.url}</a></TableCell>
+                                            <TableCell>{twitt.likes}</TableCell>
+                                            <TableCell>{isTrue ? <Button variant='contained' disabled={true}>Like</Button> : <Button color='primary' variant="contained" onClick={() => {
+                                                setLocalStore(twitt)
+                                                setInLocalStorege(twitt)
+                                            }}>like</Button>}</TableCell>
                                         </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                    {
-                                        twittList.map((twitt:any) => {
-                                            let isTrue
-                                            localStore.map((compair:any)=>{
-                                                if(twitt._id == compair._id)
-                                                {
-                                                    isTrue = true
-                                                }
-                                               
-                                            })
-                                            return (
-                                                <>
-                                                <TableRow key={twitt._id}>
-                                                    <TableCell ><Avatar src={twitt.imageUrl}/>{twitt.author}</TableCell>
-                                                    <TableCell><a href={twitt.url}>{twitt.url}</a></TableCell>
-                                                    <TableCell>{twitt.likes}</TableCell>
-                                                    <TableCell>{isTrue?<Button variant='contained' disabled={true}>Like</Button>:<Button color='primary' variant="contained" onClick={()=>{
-                                                        setLocalStore(twitt)
-                                                        setInLocalStorege(twitt)
-                                        }}>like</Button>}</TableCell>
-                                                </TableRow>
-                                                </>
+                                    </>
 
-                                                )})
-                                            }
+                                )
+                            })
+                        }
 
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                       {twittList.length==0?<div style={{textAlign:"center"}}>There is no data</div>:""}
-                       {/* {twittList.length==0?<div style={{textAlign:"center"}}>There is no data</div>:""} */}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {twittList.length == 0 ? <div style={{ textAlign: "center" }}>There is no data</div> : ""}
+            {/* {twittList.length==0?<div style={{textAlign:"center"}}>There is no data</div>:""} */}
 
-                            
-                        </div>
-                    )
-                }
+
+        </div>
+    )
+}
